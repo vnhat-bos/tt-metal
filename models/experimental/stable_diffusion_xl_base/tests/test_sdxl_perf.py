d7910b41b0fbc59474aa14885b1e150c180f873a
Nenad Babin <nbabin@tenstorrent.com>
2025-12-23 13:48:50 +0000

    Update perf of encode and decode

--- models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py
+++ models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py
@@ -113,7 +113,7 @@ def test_refiner_unet(
         ),
         (
             "pytest models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_autoencoder_kl.py::test_vae -k 'test_decode'",
-            680_239_540,
+            679_964_302,
             "sdxl_vae",
             "sdxl_vae_decode",
             VAE_DEVICE_TEST_TOTAL_ITERATIONS,
@@ -123,7 +123,7 @@ def test_refiner_unet(
         ),
         (
             "pytest models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_autoencoder_kl.py::test_vae -k 'test_encode'",
-            343_068_075,
+            340_705_479,
             "sdxl_vae",
             "sdxl_vae_encode",
             VAE_DEVICE_TEST_TOTAL_ITERATIONS,
